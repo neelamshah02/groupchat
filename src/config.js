ef862509016a7de4f105666d01446c4104ee18ef
Neelam Shah <neelam.shah@lloydsbanking.com>
2019-04-02 19:19:17 +0100

    chat app

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyDQ0CUWTLyyYaQe2JBrZtlkyGF_v28T7Ps",
+    authDomain: "chatapp-12b70.firebaseapp.com",
+    databaseURL: "https://chatapp-12b70.firebaseio.com",
+    projectId: "chatapp-12b70",
+    storageBucket: "chatapp-12b70.appspot.com",
+    messagingSenderId: "666574504390"
+  };
